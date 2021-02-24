a1a37b47cd1bdb863efb1967196f1784846edbf8
El Bekri Zineb <59539157+beniz-el@users.noreply.github.com>
2021-02-24 20:13:11 +0100

    Update Absence.cshtml.cs

--- projectASP/Pages/Absence.cshtml.cs
+++ projectASP/Pages/Absence.cshtml.cs
@@ -2,15 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using projectASP.Models;
 
 namespace projectASP.Pages
 {
     public class AbsenceModel : PageModel
     {
-        public void OnGet()
+        private readonly projectASP.Data.ASPContext _context;
+
+        public AbsenceModel(projectASP.Data.ASPContext context)
         {
+            _context = context;
         }
+
+        public IList<Absence> Absence { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            string CNE = HttpContext.Session.GetString("CNE");
+
+            var absences = from m in _context.Absences
+                        select m;
+            if (!string.IsNullOrEmpty(CNE))
+            {
+                absences = absences.Where(s => s.CNE.Contains(CNE));
+            }
+
+            Absence = await absences.ToListAsync();
+
+        }
+
     }
 }
