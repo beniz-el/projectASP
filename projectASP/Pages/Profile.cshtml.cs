7ca6420a0344662906ac39320d05e6c6afd72eba
El Bekri Zineb <59539157+beniz-el@users.noreply.github.com>
2021-02-24 20:18:58 +0100

    Update Profile.cshtml.cs

--- projectASP/Pages/Profile.cshtml.cs
+++ projectASP/Pages/Profile.cshtml.cs
@@ -5,40 +5,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using projectASP.Models;
 
 namespace projectASP.Pages
 {
     public class ProfileModel : PageModel
     {
-        
-        public string CIN { get; set; }
-        public string login { get; set; }
-        public string password { get; set; }
-        public string nom { get; set; }
-        public string prenom { get; set; }
-        public string datenaissance { get; set; }
-        public string email { get; set; }
-        public string tel { get; set; }
-        public string CNE { get; set; }
-        public string filiere { get; set; }
-        public string image { get; set; }
+       
 
-        public void OnGet()
+            private readonly projectASP.Data.ASPContext _context;
+
+        public ProfileModel(projectASP.Data.ASPContext context)
         {
-            CIN = HttpContext.Session.GetString("CIN");
-            login = HttpContext.Session.GetString("login");
-            password = HttpContext.Session.GetString("password");
-            nom = HttpContext.Session.GetString("nom");
-            prenom = HttpContext.Session.GetString("prenom");
-            datenaissance = HttpContext.Session.GetString("datenaissance");
-            email = HttpContext.Session.GetString("email");
-            tel = HttpContext.Session.GetString("tel");
-            CNE = HttpContext.Session.GetString("CNE");
-            filiere = HttpContext.Session.GetString("filiere");
-            image = HttpContext.Session.GetString("image");
-           
-
-
+            _context = context;
         }
+
+        public IList<Etudiant> Etudiant { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            string CIN = HttpContext.Session.GetString("CIN");
+            //Console.Out.WriteLine(CIN);
+            var etudiants = from m in _context.Etudiants
+                           select m;
+            if (!string.IsNullOrEmpty(CIN))
+            {
+                etudiants = etudiants.Where(s => s.CIN.Contains(CIN));
+            }
+
+           Etudiant = await etudiants.ToListAsync();
+           HttpContext.Session.SetString("CIN", CIN);
+        }
+
+
     }
 }
